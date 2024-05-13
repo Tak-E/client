5c969debe9912c388142fc16e7049a736a965409
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-05-13 16:19:44 +0900

    feat: add UserInfo page

---
+++ src/pages/userInfo/UserInfo.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import styles from "./UserInfo.module.css";
+import { useNavigate } from "react-router-dom";
+
+const UserInfo = () => {
+  const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState({
+    age: "",
+    sex: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  useEffect(() => {
+    if (errorMessage !== "") {
+      alert(errorMessage);
+    }
+  }, [errorMessage]);
+
+  const handleSubmit = () => {
+    if (userInfo.age === "" || userInfo.sex === "") {
+      setErrorMessage("나이와 성별을 모두 입력해주세요.");
+      return;
+    }
+
+    if (isNaN(userInfo.age)) {
+      setErrorMessage("나이는 숫자로 입력해주세요.");
+      return;
+    }
+
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    navigate("/");
+  };
+
+  return (
+    <section>
+      <div className={styles.container}>
+        <h1 className={styles.title}>성별과 나이를 입력해주세요.</h1>
+        <form>
+          <div className={styles.flexBox}>
+            <label htmlFor="age" className={styles.age}>
+              AGE
+            </label>
+            <input
+              type="text"
+              id="age"
+              name="age"
+              placeholder="나이를 입력하세요."
+              onChange={onChange}
+              className={styles.inputAge}
+            />
+          </div>
+          <div className={styles.select}>
+            <div>
+              <input
+                name="sex"
+                type="radio"
+                value="woman"
+                id="woman"
+                onChange={onChange}
+              />
+              <label htmlFor="woman">여자</label>
+            </div>
+            <div>
+              <input
+                name="sex"
+                type="radio"
+                id="man"
+                value="man"
+                onChange={onChange}
+              />
+              <label htmlFor="man">남자</label>
+            </div>
+          </div>
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className={styles.button}
+          >
+            선택 완료
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default UserInfo;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UserInfo from "../pages/UserInfo";
+import UserInfo from "../pages/userInfo/UserInfo";
 
 const Router = () => {
   return (
