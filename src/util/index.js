3b5583a0374e4a199b29fcc14cd25155933bf695
Taejin Kim <rlaxowls1316@naver.com>
2024-05-26 11:20:28 +0900

    feat: add extractActivitiesByCategories

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,13 @@
 export const extractActivitiesByCategory = (activities, category) => {
   return activities.filter((activity) => activity.category.includes(category));
 };
+
+export const extractActivitiesByCategories = (activities, categories) => {
+  const result = [];
+
+  categories.forEach((category) =>
+    result.push(extractActivitiesByCategory(activities, category))
+  );
+
+  return result;
+};
