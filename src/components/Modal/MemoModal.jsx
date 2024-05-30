cfdbb13116e32cc6a66ee8fc77825c00c2998118
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-05-30 22:05:47 +0900

    feat: update ActivityStats feature

--- src/components/Modal/MemoModal.jsx
+++ src/components/Modal/MemoModal.jsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 import styles from "./Modal.module.css";
 import ModalContainer from "./ModalContainer";
 
-const MemoModal = ({ onClose, id, title, memo }) => {
-  const [inputMemo, setInputMemo] = useState(memo);
+const MemoModal = ({ onClose, id, title, updateMemo }) => {
+  const [inputMemo, setInputMemo] = useState("");
 
   const handleClose = () => {
     onClose?.();
@@ -17,7 +17,21 @@ const MemoModal = ({ onClose, id, title, memo }) => {
   };
 
   const handleClick = () => {
-    console.log(memo);
+    const activities = JSON.parse(localStorage.getItem("selected-activities"));
+
+    const updatedActivities = activities.map((activity) => {
+      if (activity.id === id) {
+        return { ...activity, memo: inputMemo };
+      }
+      return activity;
+    });
+
+    localStorage.setItem(
+      "selected-activities",
+      JSON.stringify(updatedActivities)
+    );
+
+    updateMemo(id, inputMemo);
     handleClose();
   };
 
